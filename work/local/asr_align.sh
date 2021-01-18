eab79e3bd9c38655269245cec6987bc02b2a9a07
mostafashahin <mostafa_shahin@ieee.org>
2021-01-18 11:26:54 +1100

    Rempving child specific log

--- work/local/asr_align.sh
+++ work/local/asr_align.sh
@@ -24,21 +24,21 @@ do
     [ $stage -eq 3 ] || [ $stage -eq 4 ] || { echo "Run prep_lang.sh first for child $childID" ; continue; }
     for task in $tasks
     do
-        echo "asr_align: Child $childID: Start processing task $task" 2>&1 | tee -a $log
+        echo "asr_align: Child $childID: Start processing task $task" 
         [ -f $LOCAL_OUT_DIR/asr/data_$task/stage ] && task_stage=`cat $LOCAL_OUT_DIR/asr/data_$task/stage`
         echo $task_stage
         [ ! $task_stage -ge 4 ] && { echo "Seems that prep_lang.sh faild for child $childID task $task" ; all_tasks=false; continue; }
         [ ! -f $LOCAL_OUT_DIR/asr/data_$task/align_stage ] && cp $LOCAL_OUT_DIR/asr/data_$task/stage $LOCAL_OUT_DIR/asr/data_$task/align_stage
         align_stage=`cat $LOCAL_OUT_DIR/asr/data_$task/align_stage`
         if [ $align_stage -eq 4 ]; then
-            local/align_to_phone_ctm.sh  $LOCAL_OUT_DIR/asr/data_$task $MDL 2>&1 | tee -a $log || { all_tasks=false && continue; }
+            local/align_to_phone_ctm.sh  $LOCAL_OUT_DIR/asr/data_$task $MDL  || { all_tasks=false && continue; }
             align_stage=5
             echo $align_stage > $LOCAL_OUT_DIR/asr/data_$task/align_stage
         fi
 
         if [ $align_stage -eq 5 ]; then
             wget -O asr2sampa.map https://raw.githubusercontent.com/mostafashahin/aus_lexicon/main/data/asr2sampa.map
-            python3 local/ctm_to_txtgrid.py -tg $LOCAL_OUT_DIR/txtgrids/primary_16b_$task.txtgrid -p -w -l $LOCAL_OUT_DIR/asr/data_$task/dict/au_dict -m asr2sampa.map $LOCAL_OUT_DIR/asr/data_$task/align/ $LOCAL_OUT_DIR/asr/data_$task/ $LOCAL_OUT_DIR/txtgrids/primary_16b_$task.wav 2>&1 | tee -a $log || { all_tasks=false && continue; }
+            python3 local/ctm_to_txtgrid.py -tg $LOCAL_OUT_DIR/txtgrids/primary_16b_$task.txtgrid -p -w -l $LOCAL_OUT_DIR/asr/data_$task/dict/au_dict -m asr2sampa.map $LOCAL_OUT_DIR/asr/data_$task/align/ $LOCAL_OUT_DIR/asr/data_$task/ $LOCAL_OUT_DIR/txtgrids/primary_16b_$task.wav  || { all_tasks=false && continue; }
             align_stage=6
             echo $align_stage > $LOCAL_OUT_DIR/asr/data_$task/align_stage
         fi
