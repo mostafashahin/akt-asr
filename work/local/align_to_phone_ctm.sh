85f002ac5391ea36609f33fa0b0824918df9b775
mostafashahin <mostafa_shahin@ieee.org>
2021-01-26 17:05:24 +1100

    Fix using chain model

--- work/local/align_to_phone_ctm.sh
+++ work/local/align_to_phone_ctm.sh
@@ -4,6 +4,7 @@ data_dir=$1
 MDL=$2
 
 . ./path.sh
+set -e
 
 #Convert text words to upper case
 local/convert_text_case.sh $data_dir upper
@@ -13,8 +14,8 @@ steps/compute_cmvn_stats.sh $data_dir
 utils/fix_data_dir.sh $data_dir
 
 steps/online/nnet2/extract_ivectors_online.sh --nj 1 $data_dir/ $MDL/ivector_extractor/ $data_dir/ivector
-steps/nnet3/align.sh --online-ivector-dir $data_dir/ivector/ --use-gpu false --nj 1 $data_dir/ $data_dir/lang/ austalk_model_online_chain2/ $data_dir/align
+steps/nnet3/align.sh --scale-opts '--transition-scale=1.0 --acoustic-scale=1.0 --self-loop-scale=1.0' --online-ivector-dir $data_dir/ivector/ --use-gpu false --nj 1 $data_dir/ $data_dir/lang/ $MDL/ $data_dir/align
 
-ali-to-phones --ctm-output $MDL/final.mdl ark:"gunzip -c $data_dir/align/ali.1.gz |" - | utils/int2sym.pl -f 5 $MDL/phones.txt - > $data_dir/align/ali.1.ctm
+ali-to-phones --frame-shift=0.03 --ctm-output $MDL/final.mdl ark:"gunzip -c $data_dir/align/ali.1.gz |" - | utils/int2sym.pl -f 5 $MDL/phones.txt - > $data_dir/align/ali.1.ctm
 
 
